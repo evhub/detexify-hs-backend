dd4b549d9adb2e59b5ae87cf00913c092f9103c9
Evan Hubinger <evanjhub@gmail.com>
2015-10-21 19:26:46 -0700

    Customizes scripts

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-docker build -t kirel/detexify-hs-backend .
-docker run --rm --name detexify -p 3000:3000 -t kirel/detexify-hs-backend
+docker build -t evhub/detexify-hs-backend .
+docker run --rm --name detexify -p 3000:3000 -t evhub/detexify-hs-backend
